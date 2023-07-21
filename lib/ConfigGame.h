4e321a38a504bdc8e8b3af43b736d051f3347686
Starinin Andrey <starininandrey@gmail.com>
2023-07-21 20:14:43 +0300

    Add ConfigGame.h

---
+++ lib/ConfigGame.h
@@ -0,0 +1,45 @@
+#ifndef HEROES_CONFIGGAME_H
+#define HEROES_CONFIGGAME_H
+
+
+class ConfigGame {
+private:
+    static int _defenceLightArmor;
+    static int _defenceHeavyArmor;
+    static int _healthZerg;
+    static int _damageZerg;
+    static int _healthProtos;
+    static int _damageProtos;
+
+public:
+    static void Init() {
+        _defenceLightArmor = 5;
+        _defenceHeavyArmor = 10;
+        _healthZerg = 100;
+        _damageZerg = 50;
+        _healthProtos = 150;
+        _damageProtos = 75;
+    }
+
+    static int GetDefenceLightArmor() {
+        return _defenceLightArmor;
+    }
+    static int GetDefenceHeavyArmor() {
+        return _defenceHeavyArmor;
+    }
+    static int GetHealthZerg() {
+        return _healthZerg;
+    }
+    static int GetDamageZerg() {
+        return _damageZerg;
+    }
+    static int GetHealthProtos() {
+        return _healthProtos;
+    }
+    static int GetDamageProtos() {
+        return _damageProtos;
+    }
+};
+
+
+#endif //HEROES_CONFIGGAME_H
